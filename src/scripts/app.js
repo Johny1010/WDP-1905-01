5393345f53c4bb03cdda9a53b4d1a1aad650a57f
Kinga Jackowska <jackowska.kinga@gmail.com>
2019-06-03 15:43:03 +0200

    Add photo adding loop

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,1 @@
 console.log('hello');
-
-// Product photo classes loop
-var productPhotosList = document.querySelectorAll('.photo');
-for (var i = 0; i < productPhotosList.length; i++) {
-  productPhotosList[i].classList.add('photo' + (i + 1));
-}
