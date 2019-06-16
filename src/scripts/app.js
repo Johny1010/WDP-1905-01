aa93c0b2971fd84c4be4610f879de0266c95d66e
Kinga Jackowska <jackowska.kinga@gmail.com>
2019-06-16 17:34:39 +0200

    Add RWD to menu-bar

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
 console.log('hello');
+
+function toggleMenu () {
+  document.querySelector('.menu').classList.toggle('visible');
+  document.querySelector('.menu').classList.toggle('hidden');
+}
+
+document.querySelector('.menu-hamburger').addEventListener('click', function (e) {
+  e.preventDefault();
+  toggleMenu();
+});
